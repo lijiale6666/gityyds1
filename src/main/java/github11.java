7d61e3e6a677d655c77d4ddd7c1b792bd973650c
lijiale <lijiale@com>
2023-07-19 10:33:57 +0800

    yyds

--- src/main/java/github11.java
+++ src/main/java/github11.java
@@ -1,5 +1,19 @@
 public class github11 {
     public static void main(String[] args) {
         System.out.println("分享工程到github中====git bash命令推送push到远程库中");
+        int[][] x=new int[3][2];
+        x[2]=new int[1];
+        x[1]= new int[]{3, 4};
+
+        for (int i=0;i<x.length;i++){
+            for (int j=0;j<x[i].length;j++){
+                System.out.println(x[i][j]);
+            }
+        }
+        x=new int[3][];
+        for (int i=0;i< x.length;i++){
+            System.out.println(x[i]);
+        }
+
     }
 }
